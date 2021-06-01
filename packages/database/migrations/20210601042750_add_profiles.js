0b5351ed744c4ed50e5d2f0816a057b27d7f6802
Richard Foster <richard@rpf.me>
2021-06-01 05:38:00 +0100

    Switch to knex for migrations

---
+++ packages/database/migrations/20210601042750_add_profiles.js
@@ -0,0 +1,55 @@
+const sql = ([s]) => s
+
+exports.up = knex => knex.raw(sql`
+  -- Create a table for Public Profiles
+  create table profiles (
+    id uuid unique not null,
+    inserted_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+    updated_at timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+    user_id uuid references auth.users not null,
+    username text unique,
+    avatar_url text,
+    website_url text,
+
+    primary key (id),
+    unique(username),
+    constraint username_length check (char_length(username) >= 3)
+  );
+
+  alter table profiles enable row level security;
+
+  create policy "Public profiles are viewable by everyone."
+    on profiles for select
+    using ( true );
+
+  create policy "Users can insert their own profile."
+    on profiles for insert
+    with check ( auth.uid() = id );
+
+  create policy "Users can update own profile."
+    on profiles for update
+    using ( auth.uid() = id );
+
+  -- Set up Realtime!
+  begin;
+    drop publication if exists supabase_realtime;
+    create publication supabase_realtime;
+  commit;
+  alter publication supabase_realtime add table profiles;
+
+  -- Set up Storage!
+  insert into storage.buckets (id, name)
+  values ('avatars', 'avatars');
+
+  create policy "Avatar images are publicly accessible."
+    on storage.objects for select
+    using ( bucket_id = 'avatars' );
+
+  create policy "Anyone can upload an avatar."
+    on storage.objects for insert
+    with check ( bucket_id = 'avatars' );
+`)
+
+exports.down = knex => knex.raw(sql`
+  DROP TABLE profiles;
+`)
